562db8632369aed737de975ec0120e09faa26738
DanuwatY <sadnton@gmail.com>
2026-07-01 10:54:40 +0700

    add .lab2_exercise4.dart

---
+++ lab2_exercise4.dart
@@ -0,0 +1,16 @@
+void main() {
+  String? middleName;
+  String city = 'Nakhon Sawan';
+   
+  int middleNameLenght = middleName?.length ?? 0;
+  print('middle name lenght: $middleNameLenght')
+
+  print('city name lenght: ${city.length}');
+   
+  middleName ??= 'N/A' ;
+  print('Middle name after assignment: $middleName');
+
+  middleName ??= 'Someting Else';
+  print('Middle name after second assignment: $middleName')
+  
+}
